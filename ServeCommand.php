fb7b8323fa199954175f510c5af86ca61104cb85
Matt A <matthew.james.allan@gmail.com>
2016-01-16 12:48:23 -0800

    Update ServeCommand.php

--- ServeCommand.php
+++ ServeCommand.php
@@ -33,7 +33,7 @@ class ServeCommand extends Command
      */
     public function fire()
     {
-        chdir($this->laravel->publicPath());
+        chdir($this->laravel->basePath('public'));
 
         $host = $this->input->getOption('host');
 
